560e804ece796e94158fab908ae02a7194f3087d
sophiajm <sofiajmarinova@gmail.com>
2020-09-10 12:40:49 -0400

    place song list get methods in separate file

---
+++ songs/input.py
@@ -0,0 +1,24 @@
+import csv
+
+def getsong(songartist):
+    print('\n------RUNNING GETSONG')
+    # songname = input("Song title: ").strip()
+    # artistname = input("Song artist: ").strip()
+    songname = songartist[0]
+    artistname = songartist[1]
+    print(f"{songname} by {artistname}")
+
+    song = songname.lower().replace(' ','-')
+    artist = artistname.lower().capitalize().replace(' ','-')
+    print(artist+'-'+song)
+    return artist+'-'+song
+
+def getsonglist():
+    print('\n------RUNNING GETSONGLIST')
+    inputfilename = 'C:/Users/sophiajlm/Documents/DataScience_Projects/03_TopSongsWords/inputsongs.csv'
+    inputfilename = 'C:/Users/sophiajlm/Documents/DataScience_Projects/03_TopSongsWords/inputsongserrors.csv' 
+    with open(inputfilename,mode='r') as file:
+        allSongs = csv.reader(file)
+        allSongs = list(allSongs)
+    print(allSongs)
+    return allSongs
