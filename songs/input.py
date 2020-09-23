f9f39b84497e9f0d2c8e2c41ea2ba74451b2238f
sophiajm <sofiajmarinova@gmail.com>
2020-09-23 11:26:49 -0400

    change to dictionary

--- songs/input.py
+++ songs/input.py
@@ -1,11 +1,12 @@
 import csv
+import re
 
 def getsong(songartist):
     print('\n------RUNNING GETSONG')
     # songname = input("Song title: ").strip()
     # artistname = input("Song artist: ").strip()
-    songname = songartist[0]
-    artistname = songartist[1]
+    songname = re.sub('[^A-Za-z0-9 ]+','',songartist[0])
+    artistname = re.sub('[^A-Za-z0-9 ]+', '', songartist[1])
     print(f"{songname} by {artistname}")
 
     song = songname.lower().replace(' ','-')
@@ -15,7 +16,7 @@ def getsong(songartist):
 
 def getsonglist():
     print('\n------RUNNING GETSONGLIST')
-    inputfilename = 'C:/Users/sophiajlm/Documents/DataScience_Projects/03_TopSongsWords/inputsongs.csv'
+    inputfilename = 'C:/Users/sophiajlm/Documents/DataScience_Projects/03_TopSongsWords/candidates.csv'
     # inputfilename = 'C:/Users/sophiajlm/Documents/DataScience_Projects/03_TopSongsWords/inputsongserrors.csv'
     with open(inputfilename,mode='r') as file:
         allSongs = csv.reader(file)
@@ -23,7 +24,9 @@ def getsonglist():
     dicSongs = {}
     for song in allSongs:
         dicSongs[getsong(song)] = song
-    print(allSongs)
-    print(dicSongs)
+    # print(allSongs)
+    # print(dicSongs)
     # return allSongs
     return dicSongs
+
+# getsonglist()
